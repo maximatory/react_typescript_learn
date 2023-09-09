cc528786be0efe6c0f679abf9a3ae68282d085b2
Maksim <oooinfo@mail.ru>
2023-09-09 19:37:35 +0300

    feat(App.tsx): fetch async data for products from fake api

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from './components/Product';
-import { products } from './data/products';
+import axios from 'axios';
+import { Iproduct } from './models';
 
 function App() {
+  const [products, setProducts] = useState<Iproduct[]>([])
+
+  async function fetchProducts(){
+    const response = await axios.get<Iproduct[]>('https://fakestoreapi.com/products?limit=5')
+    setProducts(response.data)
+  }
+
+  useEffect(()=>{
+    fetchProducts();
+  }, [])
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
       {products.map(product=> <Product product={product} key={product.id}/>)}
