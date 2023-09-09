43256c963299c0f2f73b66f63e4451e9eb059908
Maksim <oooinfo@mail.ru>
2023-09-09 19:03:01 +0300

    feat(App.tsx): add Product components from products data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Product from './components/Product';
+import { products } from './data/products';
 
 function App() {
   return (
-    <div className="container mx-auto max-w-2xl pt-5"></div>
+    <div className="container mx-auto max-w-2xl pt-5">
+      {products.map(product=> <Product product={product} key={product.id}/>)}
+    </div>
   )
 }
 
