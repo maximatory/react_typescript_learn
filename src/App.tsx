ca1a94a3e02e26eade7dff7d183d874aff434938
Maksim <oooinfo@mail.ru>
2023-09-11 15:09:50 +0300

    feat(App.tsx): add error checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Product from './components/Product';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { Iproduct } from './models';
 
 function App() {
   const [products, setProducts] = useState<Iproduct[]>([])
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
   async function fetchProducts(){
-    setLoading(true)
-    const response = await axios.get<Iproduct[]>('https://fakestoreapi.com/products?limit=5')
-    setProducts(response.data)
-    setLoading(false)
+    try{
+      setError('')
+      setLoading(true)
+      const response = await axios.get<Iproduct[]>('https://fakestoreapi.com/products?limit=5')
+      setProducts(response.data)
+      setLoading(false)
+    } catch(e: unknown){
+      const error = e as AxiosError;
+      setLoading(false)
+      setError(error.message)
+    }
   }
 
   useEffect(()=>{
@@ -20,9 +28,8 @@ function App() {
 
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      {loading && 
-        <div>Loading...</div>
-      }
+      {loading && <div>Loading...</div>}
+      {error && <div className='text-center text-red-600 font-bold'>{error}</div>}
       {products.map(product=> <Product product={product} key={product.id}/>)}
     </div>
   )
